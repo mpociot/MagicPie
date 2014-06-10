2986f506be0ed8f15e89e9360825efe857fd825c
Fokke Zandbergen <mail@fokkezb.nl>
2014-06-10 13:55:33 +0200

    first commit

---
+++ iphone/Classes/NlFokkezbMagicpieModule.h
@@ -0,0 +1,14 @@
+/**
+ * MagicPie
+ *
+ * Created by Fokke Zandbergen
+ * Copyright (c) 2014 Fokke Zandbergen. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface NlFokkezbMagicpieModule : TiModule
+{
+}
+
+@end
